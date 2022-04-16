7e3f29fe494b3036a293040d71d56d94a6b2014f
monica-ty <acamonica@163.com>
2022-04-16 10:48:44 -0400

    feat: Add Expenses components and css

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// document.getElementById is to get root from index.html in public folder
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// replace the root div with <App />
 root.render(<App />);
